dfdc65124a3932929412f83380f6594e10ebcc2c
oljas <olja.smirnov93@gmail.com>
2023-12-25 21:22:00 +0200

    HW#56 cars service

--- src/main/java/telran/cars/service/CarsServiceImpl.java
+++ src/main/java/telran/cars/service/CarsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import telran.cars.dto.*;
+import telran.cars.exceptions.NotFoundException;
 import telran.cars.service.model.*;
 
 @Service
@@ -16,50 +17,109 @@ public class CarsServiceImpl implements CarsService {
 
 	@Override
 	public PersonDto addPerson(PersonDto personDto) {
-		// TODO Auto-generated method stub
-				return null;
+		long id = personDto.id();
+		if(owners.containsKey(id)) {
+			log.error("addPerson: owner with id {} already exist", id);
+			throw new IllegalStateException("Person with id " + id + " already exist");
+		}
+		owners.put(id, new CarOwner(personDto));
+		log.debug("addPerson: received person data: {}", personDto);
+		return owners.get(id).build();
 	}
 
 	@Override
 	public CarDto addCar(CarDto carDto) {
-		// TODO Auto-generated method stub
-				return null;
+		String number = carDto.number();
+		if(cars.containsKey(number)) {
+			log.error("addCar: car with number {} already exist", number);
+			throw new IllegalStateException("Car with number " + number + " already exist");
+		}
+		cars.put(number, new Car(carDto));
+		log.debug("addCar: received car data: {}", carDto);
+		return cars.get(number).build();
 	}
 
 	@Override
 	public PersonDto updatePerson(PersonDto personDto) {
-		// TODO Auto-generated method stub
-				return null;
+		long id = personDto.id();
+		CarOwner owner = owners.putIfAbsent(id, new CarOwner(personDto));
+		if(owner == null) {
+			log.error("updatePerson: owner with id {} doesn't exist", id);
+			throw new IllegalStateException("Person with id " + id + " doesn't exist");
+		} else {
+			log.debug("updatePerson: received person data: {}", personDto);
+		}
+		return owners.get(id).build();
 	}
 
 	@Override
 	public PersonDto deletePerson(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		CarOwner owner = owners.get(id);
+		if(owner == null) {
+			log.error("deletePerson: owner with id {} doesn't exist", id);
+			throw new NotFoundException("Person with id " + id + " doesn't exist");
+		}
+		owners.remove(id);
+		log.debug("deletePerson: person with ID {}", id);
+		return owner.build();
 	}
 
 	@Override
-	public CarDto deleteCar(String carNumber) {
-		// TODO Auto-generated method stub
-		return null;
+	public CarDto deleteCar(String number) {
+		Car car = cars.get(number);
+		if(car == null) {
+			log.error("deleteCar: car with number {} doesn't exist", number);
+			throw new NotFoundException("Person with number " + number + " doesn't exist");
+		}
+		cars.remove(number);
+		log.debug("deleteCar: car with number {}", number);
+		return car.build();
 	}
 
 	@Override
-	public TradeDealDto purchase(TradeDealDto tradeDealDto) {
-		// TODO Auto-generated method stub
-		return null;
+	public TradeDealDto purchase(TradeDealDto tradeDeal) {
+		String number = tradeDeal.carNumber();
+		long id = tradeDeal.personId();
+		if(!cars.containsKey(number) || !owners.containsKey(id)) {
+			log.error("purchase: owner with id {} or car with number {} doesn't exist", id, number);
+			throw new IllegalStateException("Person with id " + id + "or car with number " + number + " doesn't exist");
+		}
+		cars.get(number).setOwner(owners.get(id));
+		owners.get(id).getCars().add(cars.get(number));
+		log.debug("purchase: received trade deal data: {}", tradeDeal);
+		return tradeDeal;
 	}
 
 	@Override
 	public List<CarDto> getOwnerCars(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		CarOwner owner = owners.get(id);
+		if(owner == null) {
+			log.error("getOwnerCars: owner with id {} doesn't exist", id);
+			throw new IllegalStateException("Person with id " + id + " doesn't exist");
+		} 
+		List<Car> res = owner.getCars();
+		if(res.isEmpty()) {
+			log.warn("getOwnerCars: no cars for person with id {}", id);
+		} else {
+			log.trace("getOwnerCars: cars of person with id {} {}", id, res);
+		}
+		return res.stream().map(car -> new CarDto(car.getNumber(), car.getModel())).toList();
 	}
 
 	@Override
-	public PersonDto getCarOwner(String carNumber) {
-		// TODO Auto-generated method stub
-		return null;
+	public PersonDto getCarOwner(String number) {
+		Car car = cars.get(number);
+		if(car == null) {
+			log.error("deleteCar: car with number {} doesn't exist", number);
+			throw new IllegalStateException("Car with number " + number + " doesn't exist");
+		} 
+		CarOwner res = car.getOwner();
+		if(res == null) {
+			log.warn("getCarOwner: no owner of car with number {}", number);
+		} else {
+			log.debug("getCarOwner: received car number {}", number);
+		}
+		return res.build();
 	}
 
 }
--- src/test/java/telran/cars/CarsServiceTest.java
+++ src/test/java/telran/cars/CarsServiceTest.java
@@ -2,15 +2,18 @@ package telran.cars;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import telran.cars.dto.*;
+import telran.cars.exceptions.NotFoundException;
 import telran.cars.service.CarsService;
 @SpringBootTest
-class CarsServiceTset {
+class CarsServiceTest {
 	private static final String CAR_NUMBER_1 = "123-45-56";
 	private static final String MODEL_1 = "honda";
 	private static final String CAR_NUMBER_2 = "123-45-67";
@@ -24,63 +27,78 @@ class CarsServiceTset {
 	private static final String PERSON_BIRHTDATE_2 = "1990-03-20";
 	private static final String PERSON_EMAIL_2 = "kolya2@gmail.com";
 	private static final Long PERSON_ID_NOT_EXIST = 111111l;
+	private static final String CAR_NUMBER_NOT_EXIST = "120-45-50";
 	PersonDto personDto1 = new PersonDto(PERSON_ID_1, PERSON_NAME_1, PERSON_BIRHTDATE_1, PERSON_EMAIL_1);
 	PersonDto personDto2 = new PersonDto(PERSON_ID_2, PERSON_NAME_2, PERSON_BIRHTDATE_2, PERSON_EMAIL_2);
 	CarDto car1 = new CarDto(CAR_NUMBER_1, MODEL_1);
 	CarDto car2 = new CarDto(CAR_NUMBER_2, MODEL_2);
 	PersonDto personDto = new PersonDto(PERSON_ID_NOT_EXIST, PERSON_NAME_1, PERSON_BIRHTDATE_1, PERSON_EMAIL_1);
-	
+	CarDto carDto = new CarDto(CAR_NUMBER_NOT_EXIST , MODEL_2);
+	TradeDealDto tradeDeal = new TradeDealDto(CAR_NUMBER_NOT_EXIST, PERSON_ID_1);
+	TradeDealDto tradeDeal1 = new TradeDealDto(CAR_NUMBER_1, PERSON_ID_1);
+	TradeDealDto tradeDeal2 = new TradeDealDto(CAR_NUMBER_2, PERSON_ID_2);
 	@Autowired
 	CarsService carsService;
-	@BeforeEach
-	void setUp() {
-		carsService.addCar(car1);
-		carsService.addCar(car2);
-		carsService.addPerson(personDto1);
-		carsService.addPerson(personDto2);
-		carsService.purchase(new TradeDealDto(CAR_NUMBER_1, PERSON_ID_1));
-		carsService.purchase(new TradeDealDto(CAR_NUMBER_2, PERSON_ID_2));
-	}
+//	@BeforeEach
+//	void setUp() {
+//		carsService.addCar(car1);
+//		carsService.addPerson(personDto1);
+//		carsService.purchase(tradeDeal1);
+//	
+//	}
 
 	@Test
 	void testAddPerson() {
 		assertEquals(personDto, carsService.addPerson(personDto));
-		assertThrowsExactly(IllegalStateException.class, () -> carsService.addPerson(personDto1));
+		assertThrowsExactly(IllegalStateException.class, () -> carsService.addPerson(personDto));
 	}
 
 	@Test
 	void testAddCar() {
-		//TODO
+		assertEquals(carDto, carsService.addCar(carDto));
+		assertThrowsExactly(IllegalStateException.class, () -> carsService.addCar(carDto));
 	}
 
 	@Test
 	void testUpdatePerson() {
-		//TODO
+		carsService.addPerson(personDto1);
+		PersonDto personDtoUpdate = new PersonDto(PERSON_ID_1, PERSON_NAME_1, PERSON_BIRHTDATE_1, PERSON_EMAIL_2);
+		
+		carsService.updatePerson(personDtoUpdate);
 	}
 
 	@Test
 	void testDeletePerson() {
-		//TODO
+		assertEquals(personDto1, carsService.deletePerson(PERSON_ID_1));
+		assertThrowsExactly(NotFoundException.class, () -> carsService.deletePerson(PERSON_ID_NOT_EXIST));
 	}
 
 	@Test
 	void testDeleteCar() {
-		//TODO
+		assertEquals(car2, carsService.deleteCar(CAR_NUMBER_2));
+		assertThrowsExactly(NotFoundException.class, () -> carsService.deleteCar(CAR_NUMBER_NOT_EXIST));
 	}
 
 	@Test
 	void testPurchase() {
-		//TODO
+		carsService.addCar(car2);
+		carsService.addPerson(personDto2);
+		TradeDealDto actualy = carsService.purchase(tradeDeal2);
+		assertEquals(tradeDeal2, actualy);
 	}
 
 	@Test
 	void testGetOwnerCars() {
-		//TODO
+		carsService.addPerson(personDto1);
+		List<CarDto> ownerCars = carsService.getOwnerCars(PERSON_ID_1);
+		assertNotNull(ownerCars);
 	}
 
 	@Test
 	void testGetCarOwner() {
-		//TODO
+		carsService.addCar(car1);
+		carsService.purchase(tradeDeal1);
+		assertEquals(personDto1, carsService.getCarOwner(CAR_NUMBER_1));
 	}
 
 }
